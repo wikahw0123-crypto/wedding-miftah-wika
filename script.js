0e1cefc6b2a8dd6d4afaee2ffa8e2ed2b8afa532
wifna <wikahw0123@gmail.com>
2026-07-31 16:09:53 +0700

    script.js

--- script.js
+++ script.js
@@ -522,3 +522,33 @@ music.play().catch(()=>{});
 }
 
 });
+
+// ======================
+// REVEAL ON SCROLL
+// ======================
+
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll(){
+
+reveals.forEach(item=>{
+
+const windowHeight = window.innerHeight;
+
+const elementTop = item.getBoundingClientRect().top;
+
+const revealPoint = 120;
+
+if(elementTop < windowHeight - revealPoint){
+
+item.classList.add("active");
+
+}
+
+});
+
+}
+
+window.addEventListener("scroll", revealOnScroll);
+
+revealOnScroll();
